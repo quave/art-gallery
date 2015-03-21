b03cdd37e180df83da5d7e3a5e52d0dd7e04ccfd
Quave <vladsynkov@gmail.com>
2015-03-21 17:11:13 +0000

    puma run config

---
+++ config/puma.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env puma
+
+# start puma with:
+# RAILS_ENV=production bundle exec puma -C ./config/puma.rb
+
+workers ENV['SITE_WORK'].to_i || 2 
+application_path = '/var/www/art-gallery'
+railsenv = 'production'
+directory application_path
+environment railsenv
+daemonize true
+pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
+state_path "#{application_path}/tmp/pids/puma-#{railsenv}.state"
+stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
+threads 4, 16
+bind "unix:///var/tmp/art-gallery.sock"
+
+preload_app!
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
